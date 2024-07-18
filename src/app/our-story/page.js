5e5092cb60c3378e94d402a0cadc45b5bf9a26b8
manishaverma15 <manishaverma15873@gmail.com>
2024-07-18 19:41:07 +0530

    feat: add our story page

---
+++ src/app/our-story/page.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const OurStory = () => {
+  return (
+    <>
+      <section class="bg-white dark:bg-gray-900">
+        <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+          <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">We didn't reinvent the wheel</h2>
+            <p class="mb-4">Decades ago, as a little boy.. growing up in Karnal I started my tryst with this beautiful life.
+              Born and brought up in a family that is a curious combination of modern and traditional values. These people are my  biggest support for the very fact that they always let me follow my heart's content.
+              Looking back to my life and it's adolescent years, the most important thing I realize was my curiosity to know every little detail about the toys I had. I just love creating something out of nothing and that's where I realized choosing a career into creativity is something I exactly fit in.</p>
+            <p class="mb-4">Studying science was much a tradition in the clan and  then there's is this peer pressure, so I just got into it.
+              Dutifully, i took up B.E. specializing in computer science. But soon after opting for B.tech I realized, i don't fit in there.
+              As they say, it's never too late to follow your heart's content.</p>
+            <p class="mb-4">I believe in a little saying that you either suit up well, go to your job, do what everyone else is doing.. or pave your own way, take risks, break stereotypes, swim upstream like a trout against norms and then whatever little that you achieve is yours.
+              And so I have come up with opting this as my career. I believe in discovering something out of nothing and this business surely makes me feel that way.</p>
+            <p>Please let me know about any flaws that you come across. Also let me know what I can do to make this place more useful to you.
+              You can contact me for feedback, regarding reviews or anything. All suggestions are most welcome !
+              Just scroll down the page and fill a short form to get in touch with me !</p>
+          </div>
+          <div class="grid grid-cols-2 gap-4 mt-8">
+            <img class="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1" />
+            <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2" />
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+export default OurStory;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -8,7 +8,7 @@ const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header>
+    <header className="sticky top-0 z-50">
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <a className="flex items-center">
@@ -38,13 +38,13 @@ const Header = () => {
           <div className={`${isOpen ? 'block' : 'hidden'} justify-between items-center w-full lg:flex lg:w-auto lg:order-1`} id="mobile-menu-2">
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
               <li>
-                <a href= '/' className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Home</a>
+                <a href='/' className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Home</a>
               </li>
               <li>
                 <a className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Customized Work</a>
               </li>
               <li>
-                <a className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Our Story</a>
+                <Link href={{ pathname: '/our-story' }} className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Our Story</Link>
               </li>
               <li>
                 <Link href={{ pathname: '/contact-us' }} className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Contact</Link>
