6ec9b4b745067232c6cf0b89c30150a5da60a737
manishaverma15 <manishaverma15873@gmail.com>
2024-07-22 17:22:41 +0530

    feat: add customized workr

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,13 @@ import HeroSection from "@/components/hero/hero";
 import WhyUs from "@/components/whyUs/whyUs";
 import Reviews from "@/components/reviews/reviews";
 import FAQ from "@/components/faq/faq";
+import Category from "@/components/work-category/work-category";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <Category />
       <WhyUs />
       <Reviews />
       <FAQ />
---
+++ src/components/work-category/work-category.js
@@ -0,0 +1,33 @@
+"use client"
+
+import React from 'react';
+import Link from 'next/link';
+
+const categories = [
+  { name: 'Lighted Nameplates', image: '/images/lighted-name-plate.jpg', link: '/category/lighted-nameplates' },
+  { name: 'Non-Lighted Nameplates', image: '/images/non-lighted-nameplate.jpg', link: '/category/non-lighted-nameplates' },
+  { name: 'Neon Signs', image: '/images/neon-signs.jpg', link: '/category/neon-signs' },
+  { name: 'Pillar/Gate Lamps', image: '/images/pillar-gate-lamps.png', link: '/category/pillar-gate-lamps' },
+];
+
+const Category = () => {
+  return (
+    <div className="container mx-auto text-center">
+      <h1 className="text-4xl font-bold my-8">Customized Work Gallery</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        {categories.map((category) => (
+          <Link href={category.link} key={category.name}>
+            <div className="relative w-full h-72 cursor-pointer">
+              <img src={category.image} alt={category.name} className="w-full h-full object-cover" />
+              <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
+                <div className="text-white text-xl">{category.name}</div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Category;
